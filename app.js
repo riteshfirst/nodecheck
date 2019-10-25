7bf5b458edea1c4bb2c8af1aa869002cb7f19e05
Srivastava <Ritesh.Srivastava@TheAA.com>
2019-10-25 13:11:51 +0100

    Adding sql

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const express =require ('express') ;
-
+const sql = require ('mssql') ;
+var url = require('url');
 const app= express() ;
+
+
 
 app.use ('/', function(req, res )
 {
-res.send ('Hello Test' ) ;
+//res.send ('Hello Test' ) ;
+   res.header("Access-Control-Allow-Origin", "*");
+    // config for your database
+var config = {
+        user: 'testadm',
+        password: 'test',
+        server: 'testserver', 
+        database: 'testdb' 
+    };
+	
+var q = url.parse(req.url, true).query;
+res.send (q.name ) ;
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query("select * from testtbl" , function (err, recordset) {
+            
+            if (err) console.log(err)
+console.log(recordset);
+            // send records as a response
+            res.send(recordset);
+            sql.close()
+        });
+    });
+
+
+
 
 }) ;
 
